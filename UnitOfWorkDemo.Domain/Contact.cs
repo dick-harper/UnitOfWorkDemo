e990b8585ddf7460ec085c83495ead755db97307
Dick Harper <dick.harper@gmail.com>
2017-01-14 08:51:35 -0700

    Add contact entity.

---
+++ UnitOfWorkDemo.Domain/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnitOfWorkDemo.Domain
+{
+    public class Contact : Entity<int>
+    {
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastNameName { get; set; }
+        public virtual string MiddleName { get; set; }
+    }
+}
