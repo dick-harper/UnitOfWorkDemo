1cb9c846ffa982cd1af8b15c20ae01eea0c3619b
Dick Harper <dick.harper@gmail.com>
2017-01-16 04:25:30 -0700

    Work on session factory.

---
+++ UnitOfWorkDemo.Repository.NHIbernate/NHibernateSessionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NHibernate;
+
+namespace UnitOfWorkDemo.Repository.NHIbernate
+{
+    public sealed class NHibernateSessionFactory
+    {
+        private static readonly Lazy<NHibernateSessionFactory> Lazy =
+        new Lazy<NHibernateSessionFactory>(() => new NHibernateSessionFactory());
+
+        public static NHibernateSessionFactory Instance => Lazy.Value;
+
+        public ISession OpenSession()
+        {
+            return null;
+        }
+
+        private NHibernateSessionFactory()
+        {
+        }
+    }
+}
